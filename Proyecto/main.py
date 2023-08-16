3288d4191a28ba3f4b3f1e8c48b4ea188cb41bd1
Augusto Guchea <aguchealezama@gmail.com>
2023-08-16 15:47:20 -0300

    Cambio 16 agosto 1.1

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,55 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
+from Funciones import OPCIONES_MODOS
+
+modo_var = None
+def Ventana_Inicio():
+    global modo_var
+    ventana = tk.Tk()
+    ventana.title("Preguntados")
+    ventana.geometry("1000x800")  
+    ventana.config(bg="gray")
+    ventana.config(bd=25)
+    ventana.config(relief="groove")
+
+    frame_principal = tk.Frame(ventana, bg="gray") 
+    frame_principal.pack(expand=True)
+
+    etiqueta_titulo = tk.Label(frame_principal, text="Elige un modo:", font=("Arial", 24), bg="gray")
+    etiqueta_titulo.pack(pady=20)
 
 
-ventana = tk.Tk()
-ventana.title("Preguntados")
-ventana.geometry("1000x800")  
-ventana.config(bg="gray")
-ventana.config(bd=25)
-ventana.config(relief="groove")
 
-frame_principal = tk.Frame(ventana, bg="gray") 
-frame_principal.pack(expand=True)
+    modo_var = tk.StringVar()
+    modo_var.set(OPCIONES_MODOS[0])  
+    menu_desplegable = ttk.Combobox(frame_principal, values=OPCIONES_MODOS, textvariable=modo_var, state="readonly", font=("Arial", 18), width=17)
+    menu_desplegable.pack(pady=10)
 
-etiqueta_titulo = tk.Label(frame_principal, text="Elige un modo:", font=("Arial", 24), bg="gray")
-etiqueta_titulo.pack(pady=20)
+    def iniciar_juego():
+        modo_seleccionado = modo_var.get()
+        print("Modo seleccionado:", modo_seleccionado)
+        ventana.destroy()
+        
+    boton_iniciar = tk.Button(frame_principal, text="Iniciar juego", command=iniciar_juego, font=("Arial", 20), padx=20, pady=10, bg="pink")
+    boton_iniciar.pack(pady=20)
 
+    def Salir():
+        print("Saliendo del juego...")
+        ventana.destroy()
+        return exit()
 
-opciones_modos = ['Preguntas Aleatorias',
-                "Eleccion de Categoria",
-            'De a 2 jugadores',
-            ]
+    boton_salir = tk.Button(frame_principal, text="Salir", command=Salir, font=("Arial", 15), padx=10, pady=10, bg="red")
+    boton_salir.pack(pady=10)
 
-
-modo_var = tk.StringVar()
-modo_var.set(opciones_modos[0])  
-menu_desplegable = ttk.Combobox(frame_principal, values=opciones_modos, textvariable=modo_var, state="readonly", font=("Arial", 18), width=17)
-menu_desplegable.pack(pady=10)
-
-def iniciar_juego():
-    modo_seleccionado = modo_var.get()
-    print("Modo seleccionado:", modo_seleccionado)
-    ventana.destroy()
-    
-boton_iniciar = tk.Button(frame_principal, text="Iniciar juego", command=iniciar_juego, font=("Arial", 20), padx=20, pady=10, bg="pink")
-boton_iniciar.pack(pady=20)
-
-def Salir():
-    print("Saliendo del juego...")
-    ventana.destroy()
-    return exit()
-
-boton_salir = tk.Button(frame_principal, text="Salir", command=Salir, font=("Arial", 15), padx=10, pady=10, bg="red")
-boton_salir.pack(pady=10)
-
-frame_principal.update_idletasks()
-width = frame_principal.winfo_width()
-height = frame_principal.winfo_height()
-x = (ventana.winfo_screenwidth() // 2) - (width // 2)
-y = (ventana.winfo_screenheight() // 2) - (height // 2)
-
-
+    frame_principal.update_idletasks()
+    width = frame_principal.winfo_width()
+    height = frame_principal.winfo_height()
+    x = (ventana.winfo_screenwidth() // 2) - (width // 2)
+    y = (ventana.winfo_screenheight() // 2) - (height // 2)
+    return ventana.mainloop()
 
 
 import random
@@ -60,8 +57,9 @@ from tkinter import messagebox
 if __name__ == "__main__":
     incorrectos = 0
     correctos = 0
-    ventana.mainloop()
+    Ventana_Inicio()
     modo_seleccionado = modo_var.get()
+    preguntas_hechas = {categoria: [] for categoria in CATEGORIAS}
     
     if modo_seleccionado == 'Preguntas Aleatorias':
         print(f"")
@@ -69,7 +67,7 @@ if __name__ == "__main__":
         while correctos < 10 and incorrectos < 3:
             cat_elegida = random.randint(0, len(CATEGORIAS) - 1)
             
-            respuesta = preguntas(CATEGORIAS[cat_elegida])
+            respuesta = preguntas(CATEGORIAS[cat_elegida], preguntas_hechas)
 
             if respuesta:
                 messagebox.showinfo( "Correcto", "Respuesta Correcta ")
@@ -139,7 +137,7 @@ if __name__ == "__main__":
         print(f"")
         print(f"")
         info_jugador = {x: False for x in CATEGORIAS}
-        eleccion_cat(correctos,incorrectos, info_jugador)
+        eleccion_cat(correctos,incorrectos, info_jugador, preguntas_hechas)
         
         
-"comentario"
+            
